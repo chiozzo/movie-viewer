51a44244bb5952293bc1a7b0a36059abd72c4476
Brian Merchant <btmerchant@comcast.net>
2015-10-17 13:25:31 -0500

    Cant get handlebars working

--- javascripts/main.js
+++ javascripts/main.js
@@ -19,10 +19,12 @@ requirejs.config({
   }
 });
 
-require(["jquery", "q", "search", "lodash", "bootstrap", "material"], function($, q, search, _, bootstrap, material) {
+require(["jquery", "search", "q", "lodash", "bootstrap", "material",  "hbs!../templates/movie"], function($, search, q, _, bootstrap, movieHB) {
 
 //Initialize material design for project
+
   $.material.init();
+
 
 //this toggles the modal window to 'shown' and 'hidden' when the user clicks on the element with the id of 'login'
   $('#login').on('click', function () {
@@ -33,8 +35,47 @@ require(["jquery", "q", "search", "lodash", "bootstrap", "material"], function($
 //this code will be used to grab the user input for the search bar.  The variable will then be injected/ concatenated into the ajax request url.
   $('#send').click(function(movie){
     search.result()
-      .then(function(movie){
-        console.log("movie", movie);
+      .then(function(movieData){
+        console.log("movieData", movieData);
+        //turn promise data into an array to use with handlebars
+        $("#listBox").append(movieHB({movieItem: movieData}));
+        // $("#listBox").append("Test");
+        console.log("dom Write");
+
+
+
+
+
+// requirejs(
+//   ["jquery", "hbs", "bootstrap", "get-books", "xhr1", "q", "filter"],
+//   function($, Handlebars, bootstrap, books, x1, q, filter) {
+//     var allBooks;
+//     books.loadBooks();
+//     .then(function(books){
+//       books = Object.keys( books ).map(key => books[ key ]);
+//       allBooks = books;
+//       return x1.bookType();
+//     })
+//     .then(function(types) {
+//       types = Object.keys( types ).map(key => types[ key ]);
+//       var bookArray = allBooks.map(book => {
+//       book.type = _.find(types, { id:book.booktype }).label;
+//       return book;
+//       });
+//       require(['hbs!../templates/genres'], function(genreTpl) {
+//         $("#bookList").prepend(genreTpl({types: types}));
+//       });
+//       require(['hbs!../templates/books'], function(bookTpl) {
+//       $("#bookList").html(bookTpl({books:bookArray}));
+//         $(document).on('change', "#genres", function(){
+//           filter.byGenre();
+//        });
+
+
+
+
+
+
 
         //this will save the movie object to our firebase account
         // $.ajax({
