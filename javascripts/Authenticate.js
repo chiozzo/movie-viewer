cdfce86dbb5a7945bc844d2197f35244474e260f
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-10-22 22:11:03 -0500

    cleaned up comments from login area and uncommented their code to populate the soon to be data in the dom

--- javascripts/Authenticate.js
+++ javascripts/Authenticate.js
@@ -14,12 +14,43 @@ return {
         console.log("Login Failed!", error);
       } else{
         console.log("login successful");
+
+// =========above is working
+
+        console.log("Authenticated successfully with payload:", authData);
+        getUsers.setUid(authData.uid);
+        getUsers.load(authData.uid);
+        $("#inputEmail").val('');
+        $("#inputPassword").val('');
+        //show user input on successful load to interact with app
+        
+        $("#user_input").show();
+        $("#send").show();
+        //show firebase snapshot on load based on authData Uid
+        firebaseRef.child("users/" + authData.uid + "/movies/").on("value", function(snapshot){
+          var movies = snapshot.val();
+          console.log("movies", movies);
+          allMoviesArray = [];
+          
+          for (var key in movies){
+            var movieWithId = movies[key];
+            movieWithId.key = key;
+            console.log("movieWithId", movieWithId);
+            allMoviesArray[allMoviesArray.length] = movieWithId;
+          }
+
+          allMoviesObject = {movie : allMoviesArray};
+
+          originalMoviesArray = allMoviesArray.slice();
+
+          $("#movie").html(templates.movie(allMoviesObject));
+        });
       }
     })
   },
 
 
-// ==================
+// ==================below registers user
 
     getRegister: function(){
       var newUserEmail = $('#inputEmail').val();
@@ -41,40 +72,3 @@ return {
   };
 }); 
 
-// commenting their code out while getting login to work
-
-        // console.log("Authenticated successfully with payload:", authData);
-        // getUsers.setUid(authData.uid);
-        // getUsers.load(authData.uid);
-        // $("#inputEmail").val('');
-        // $("#inputPassword").val('');
-        // //show user input on successful load to interact with app
-        
-        // $("#user_input").show();
-        // $("#send").show();
-        // //show firebase snapshot on load based on authData Uid
-        // firebaseRef.child("users/" + authData.uid + "/movies/").on("value", function(snapshot){
-        //   var movies = snapshot.val();
-        //   console.log("movies", movies);
-
-        //   allMoviesArray = [];
-
-        //   for (var key in movies){
-        //     var movieWithId = movies[key];
-        //     movieWithId.key = key;
-        //     console.log("movieWithId", movieWithId);
-        //     allMoviesArray[allMoviesArray.length] = movieWithId;
-        //   }
-
-        //   allMoviesObject = {movie : allMoviesArray};
-
-        //   originalMoviesArray = allMoviesArray.slice();
-
-        //   $("#movie").html(templates.movie(allMoviesObject));
-
-//         });
-//       }
-// 	   });
-// 	 }
-//   };
-// });
