aa9ce652144252386b91c8db6e09da82481e8782
Jon Huffaker <jonathanhuffaker@gmail.com>
2015-10-23 23:58:17 -0500

    commiting changes before pulling from master

--- javascripts/Authenticate.js
+++ javascripts/Authenticate.js
@@ -1,74 +1,77 @@
 define(["jquery", "firebase", "getUsers", "movieTemplates"],
 	function($, firebase, getUsers, templates) {
 
-var firebaseRef = new firebase("https://movie-viewe.firebaseio.com");
+    var firebaseRef = new firebase("https://movie-viewe.firebaseio.com");
 
-return {
-  //this logs user into firebase based on email and password
-	logInUser: function(email, password) {
-		firebaseRef.authWithPassword({
-      email: $("#inputEmail").val(),
-      password: $("#inputPassword").val()
-    }, function(error, authData) {
-      if (error) {
-        console.log("Login Failed!", error);
-      } else{
-        console.log("login successful");
-
-// =========above is working
-
-        console.log("Authenticated successfully with payload:", authData);
-        getUsers.setUid(authData.uid);
-        getUsers.load(authData.uid);
-        $("#inputEmail").val('');
-        $("#inputPassword").val('');
-        //show user input on successful load to interact with app
-        
-        $("#user_input").show();
-        $("#send").show();
-        //show firebase snapshot on load based on authData Uid
-        firebaseRef.child("users/" + authData.uid + "/movies/").on("value", function(snapshot){
-          var movies = snapshot.val();
-          console.log("movies", movies);
-          allMoviesArray = [];
-          
-          for (var key in movies){
-            var movieWithId = movies[key];
-            movieWithId.key = key;
-            console.log("movieWithId", movieWithId);
-            allMoviesArray[allMoviesArray.length] = movieWithId;
-          }
-
-          allMoviesObject = {movie : allMoviesArray};
-
-          originalMoviesArray = allMoviesArray.slice();
-
-          $("#movie").html(templates.movie(allMoviesObject));
-        });
-      }
-    })
-  },
-
-
-// ==================below registers user
-
-    getRegister: function(){
-      var newUserEmail = $('#inputEmail').val();
-      firebaseRef.createUser({
-        email    : newUserEmail,
-        password : $('#inputPassword').val()
-      }, function(error, userData) {
+    return {
+      //this logs user into firebase based on email and password
+    	logInUser: function(email, password) {
+    		firebaseRef.authWithPassword({
+          email: $("#inputEmail").val(),
+          password: $("#inputPassword").val()
+        }, function(error, authData) {
           if (error) {
-            console.log("Error creating user:", error);
-          } else {
-            var newUser ={
-              userEmail: newUserEmail
-            };
-            firebaseRef.child('users').child(userData.uid).set(newUser);
-            $('#loginMessage').text(newUserEmail + " is now registered. Please login.");
+            console.log("Login Failed!", error);
+          } else{
+            console.log("login successful");
+
+    // =========above is working
+
+            console.log("Authenticated successfully with payload:", authData);
+            getUsers.setUid(authData.uid);
+            getUsers.load(authData.uid);
+            $("#inputEmail").val('');
+            $("#inputPassword").val('');
+            //show user input on successful load to interact with app
+            
+            $("#user_input").show();
+            $("#send").show();
+            //show firebase snapshot on load based on authData Uid
+            firebaseRef.child("users/" + authData.uid + "/movies/").on("value", function(snapshot){
+              var movies = snapshot.val();
+              console.log("movies", movies);
+              allMoviesArray = [];
+              
+              for (var key in movies){
+                var movieWithId = movies[key];
+                movieWithId.key = key;
+                console.log("movieWithId", movieWithId);
+                allMoviesArray[allMoviesArray.length] = movieWithId;
+              }
+
+              allMoviesObject = {movie : allMoviesArray};
+
+              originalMoviesArray = allMoviesArray.slice();
+
+              $("#movie").html(templates.movie(allMoviesObject));
+            });
           }
-      });
-    }
-  };
-}); 
+        })
+      },
+
+
+    // ==================below registers user
+
+        getRegister: function(){
+          var newUserEmail = $('#inputEmail').val();
+          firebaseRef.createUser({
+            email    : newUserEmail,
+            password : $('#inputPassword').val()
+            }, function(error, userData) {
+                if (error) {
+                  console.log("Error creating user:", error);
+                  } else {
+                    var newUser ={
+                      userEmail: newUserEmail
+                    };
+                    firebaseRef.child('users').child(userData.uid).set(newUser);
+                    $('#loginMessage').text(newUserEmail + " is now registered. Please login.");
+                  }
+                })
+              }
+        }
+
+
+  });
+ 
 
--- javascripts/search.js
+++ javascripts/search.js
@@ -1,48 +1,52 @@
-define(function(require) {
-  var $ = require("jquery");
-  var q = require("q");
-  var firebase = require("firebase");
+define(["jquery", "q", "firebase"]),
+  function($, q, firebase) {
+    // ++++added firebase ref below
 
-  return {
-    //omdb search functionality
-    omdb : function() {
-      //declare var for userInput
-      var userInput = $("#user_input").val();
-      var deferred = q.defer();
-      console.log("userInput", userInput);
-      //split and join userInput to inject into url in a format readable for api
-      userInput = userInput.split(" ").join("+");
-      console.log("user input +", userInput);
-      //ajax call using promises
-      $.ajax({ url : "http://www.omdbapi.com/?s=" + userInput + "&y=&plot=short&r=json" }).done(function(movie){
-        console.log("movie", movie);
-        //return movie object Search key value
-        deferred.resolve(movie.Search);
-      })
-    .fail(function(xhr, status, error){
-      deferred.reject(error);
-    });
-    return deferred.promise;
-    },
-    //search user library WIP
-    myLib : function(){
-      var searchInput = $("#user_input").val();
+    var firebaseRef = new firebase("https://movie-viewe.firebaseio.com");
+    return {
+      //omdb search functionality
+      omdbSearch: function(searchString) {
+        var deferred = q.defer();
+        //declare var for userInput
+        var userInput = $("#user_input").val();
+        console.log("userInput", userInput);
+        //split and join userInput to inject into url in a format readable for api
+        userInput = userInput.split(" ").join("+");
+        console.log("user input +", userInput);
+        //ajax call using promises
+        $.ajax({ url : "http://www.omdbapi.com/?s=" + userInput + "&y=&plot=short&r=json" }).done(function(movie){
+          console.log("movie", movie);
+          //return movie object Search key value
+          deferred.resolve(movie.Search);
+        })
+      .fail(function(xhr, status, error){
+        deferred.reject(error);
+      });
+      return deferred.promise;
+      },
 
-    },
-    //add movie to specific users library
-    addMovie : function(imdbID) {
-      //declare var for userInput
-      var deferred = q.defer();
-      //ajax call using promises
-      $.ajax({ url : "http://www.omdbapi.com/?i=" + imdbID + "&y=&plot=short&r=json" }).done(function(myMovie){
-        console.log("myMovie", myMovie);
-        //return movie object Search key value
-        deferred.resolve(myMovie);
-      })
-    .fail(function(xhr, status, error){
-      deferred.reject(error);
-    });
-    return deferred.promise;
-    }
+      //search user library WIP
+      myLib : function(){
+        var searchInput = $("#user_input").val();
+      },
+
+
+      //add movie to specific users library
+      addMovie : function(imdbID) {
+        //declare var for userInput
+        var deferred = q.defer();
+        //ajax call using promises
+        $.ajax({ url : "http://www.omdbapi.com/?i=" + imdbID + "&y=&plot=short&r=json" }).done(function(myMovie){
+          console.log("myMovie", myMovie);
+          //return movie object Search key value
+          deferred.resolve(myMovie);
+        })
+        .fail(function(xhr, status, error){
+          deferred.reject(error);
+        });
+        return deferred.promise;
+      }
+
+    };
   };
-});
+  
